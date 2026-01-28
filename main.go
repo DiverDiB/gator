dedd87fc986943a94983965f3ee2b04cec13721b
DiverDiB <diverdib@gmail.com>
2026-01-28 01:22:03 -0600

    Partial implementation of fetchFeed

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"encoding/xml"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"time"
+
 	"github.com/diverdib/gator/internal/config"
+	"github.com/diverdib/gator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,6 +29,21 @@ type command struct {
 
 type commands struct {
 	registeredCommands map[string]func(*state, command) error
+}
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
 }
 
 // register adds a new handler function to the map
@@ -38,18 +64,114 @@ func (c *commands) run(s *state, cmd command) error {
 }
 
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return fmt.Errorf("the login handler expects a single argument, the username")
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.name)
 	}
-	username := cmd.args[0]
+	name := cmd.args[0]
 
-	err := s.cfg.SetUser(username)
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("user %s does not exist", name)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("could not set current user: %w", err)
 	}
 
-	fmt.Printf("User has been set to: %s\n", username)
+	fmt.Printf("User has been set to: %s\n", name)
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.name)
+	}
+
+	name := cmd.args[0]
+
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("could not create user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("could not set current user: %w", err)
+	}
+
+	fmt.Printf("User %s was created successfully!\n", user.Name)
+	fmt.Println(user)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.ResetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("could not reset users: %w", err)
+	}
+	fmt.Println("All users have been deleted from the database.")
+	return nil
+}
+
+func handlerGetUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("could not get users: %w", err)
+	}
+	currentUser := s.cfg.CurrentUserName
+	for _, user := range users {
+		if user.Name == currentUser {
+			fmt.Printf("* %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %s\n", user.Name)
+		}
+	}
+	return nil
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the User-Agent header
+	req.Header.Set("User-Agent", "gator")
+
+	// Make the HTTP request
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Check for non-200 status codes
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch feed: status code %d", resp.StatusCode)
+	}
+
+	// Read the data and Unmarshal into RSSFeed struct
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var feed RSSFeed
+	err = xml.Unmarshal(data, &feed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &feed, nil
 }
 
 func main() {
@@ -60,7 +182,16 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DbURL)
+	if err != nil {
+		log.Fatalf("error opening database: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
+	// Create the program state
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
@@ -69,6 +200,9 @@ func main() {
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerGetUsers)
 
 	// Check if enough argumaents were provided
 	if len(os.Args) < 2 {
@@ -83,7 +217,7 @@ func main() {
 
 	// Run the command
 	err = cmds.run(programState, cmd)
-	if err!= nil {
+	if err != nil {
 		log.Fatalf("error running command: %v", err)
 	}
 
